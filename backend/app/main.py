6f4b95560550cbae8859e7767a4df809625d4d66
Tom C <78508475+tc10815@users.noreply.github.com>
2023-09-27 21:26:26 -0400

    Trying to get backend talking to frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"], # Add your frontend's URL here
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
